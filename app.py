cd425ee1426cd8b7009ea780f7079da0c4b4e781
2633aiien <72729774+2633aiien@users.noreply.github.com>
2021-12-02 18:03:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
+bot_info = line_bot_api.get_bot_info()
+
+print(bot_info.display_name)
+print(bot_info.user_id)
+print(bot_info.basic_id)
+print(bot_info.premium_id)
+print(bot_info.picture_url)
+print(bot_info.chat_mode)
+print(bot_info.mark_as_read_mode)
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
@@ -33,8 +42,15 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
 
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    if(event.message.text == "醫院資訊"):
+        message = TextSendMessage("醫院資訊")
+    if(event.message.text == "我要預約"):
+        message = TextSendMessage("我要預約")
+    if(event.message.text == "資訊"):
+        message = TextSendMessage(str(bot_info.user_id))
+    if(event.message.text == "我"):
+        message = TextSendMessage(bot_info.user_id)
+    line_bot_api.reply_message(
+        event.reply_token,
+        message)
